14ad275162316ae48d62c654c4fe3e9fd3b6abee
User <User@DESKTOP-A3NEN4N>
2023-02-02 15:06:57 +0300

    Добавьте файлы проекта.

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test
+{
+    public partial class MainWindow : Window
+    {
+        List<TestView> TestViews = new List<TestView>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 1; i < Core.DB.User.Count() + 1; i++)
+            {
+                var user = Core.DB.User.Where(u => u.Id == i).FirstOrDefault();
+                var ent = new TestView(user.Name, user.Description, user.Picture, user.Manufacturer, user.Cost, user.Count);
+                TestViews.Add(ent);
+            }
+
+            LV.ItemsSource = TestViews;
+        }
+    }
+}
---
+++ Test/TestView.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Test
+{
+    public class TestView
+    {
+        public string Color { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        public string Manufacturer { get; set; }
+        public string Cost { get; set; }
+        public int Count { get; set; }
+
+        public TestView(string name, string desc, string pic, string manufact, string cost, int count)
+        {
+            if (pic != null)
+            {
+                Picture = "/Images/" + pic;
+            }
+            else
+            {
+                Picture = "/Images/null.png";
+            }
+
+            Name = name;
+            Description = desc;
+            Manufacturer = manufact;
+            Cost = cost;
+            Count = count;
+
+            if (count == 0)
+            {
+                Color = "Gray";
+            }
+            else
+            {
+                Color = "White";
+            }
+        }
+    }
+}
